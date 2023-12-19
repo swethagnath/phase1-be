78423372f454693bdd27da06bce3f12b1bcb7321
swetha.gopinath@texple.com <swethagnthn@gmail.com>
2023-12-19 21:10:03 +0530

    finished route logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -59,4 +59,11 @@ class AuthController extends Controller
         return response($response, 201);
     }
 
+    public function logout(Request $request){
+        auth()->user()->tokens()->delete();
+        return [
+            'message' => 'Logged Out'
+        ];
+    }
+
 }
